badf1c01b19a10902b888d1b737b2d8fa2a62663
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2024-01-10 09:55:42 +0100

    Up to date

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { MainLayout } from './Layout/MainLayout'
+import Homepage from './Layout/Homepage'
+import Pallette from './Layout/Pallette'
 
 function App() {
  
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<MainLayout/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/palletes" element={<Pallette />} />
+      </Routes>
     </BrowserRouter>
   )
 }
