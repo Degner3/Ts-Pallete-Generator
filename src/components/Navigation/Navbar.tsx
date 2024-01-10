8552fa3c2765e8583ed3e71dda6d0a3c6d0225f2
Degner3 <niklaschristiansen@live.dk>
2024-01-10 14:12:28 +0100

    Navigation colorchange

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,22 +1,49 @@
 import { NavLink } from "react-router-dom";
 import style from "./Navbar.module.scss";
+import { useColors } from "../../store/useColors";
+
 
 export default function Navbar() {
+  const { currentColors } = useColors();
+
   const navArr = [
     { link: "/", page: "Random Pallette" },
     { link: "pallete", page: "My Palettes" },
   ];
 
+
+
   return (
     <nav className={style.nav}>
       <div className={style.navwrapper}>
-        <h2>Colorizer Pallete Genereator</h2>
+        <h2
+          key={currentColors ? ` colored ${currentColors[0]}` : "not colored"}
+          style={{
+            background:
+              currentColors && currentColors.length >= 2
+                ? `-webkit-linear-gradient(0deg, ${currentColors[0]} 0%, ${currentColors[4]} 100%)`
+                : "white",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+        >
+          Colorizer Pallete Genereator
+        </h2>
         <ul>
           {navArr.map((item, i) => (
             <li key={i}>
               <NavLink
                 to={item.link}
-                // activeClassName={style.active}
+                style={{
+                  backgroundColor: "transparent",
+                  color: "#ededed",
+                  fontSize: "16px",
+                  padding: "6px 15px",
+                  border: "2px solid transparent",
+                  borderImage: currentColors[0]
+                    ? `linear-gradient(to right, ${currentColors[0]}, ${currentColors[4]}) 1`
+                    : "white",
+                }}
               >
                 {item.page}
               </NavLink>
