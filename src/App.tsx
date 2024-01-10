f6be6c5f0ca0d6f3b5dae3ad1c068c6ee728226d
Degner3 <niklaschristiansen@live.dk>
2024-01-10 12:01:59 +0100

    Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 import { MainLayout } from './Layout/MainLayout'
 import Homepage from './Layout/Homepage/Homepage'
 import Pallette from './Layout/Pallette/Pallette'
+import Navbar from './components/Navigation/Navbar'
 
 function App() {
  
 
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Homepage/>} />
         <Route path="/palletes" element={<Pallette />} />
--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
+import style from "./Navbar.module.scss";
 
 export default function Navbar() {
+  const navArr = [
+    { link: "/", page: "Random Pallette" },
+    { link: "pallete", page: "My Palettes" },
+  ];
+
   return (
-    <div>Navbar</div>
-  )
+    <nav className={style.nav}>
+      <div className={style.navwrapper}>
+        <h2>Colorizer Pallete Genereator</h2>
+        <ul>
+          {navArr.map((item, i) => (
+            <li key={i}>
+              <NavLink
+                to={item.link}
+                // activeClassName={style.active}
+              >
+                {item.page}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 }
