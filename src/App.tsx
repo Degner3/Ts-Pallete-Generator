a2e8da3b75c8c9b0d28966deae369dea1e3bbc13
Degner3 <niklaschristiansen@live.dk>
2024-01-10 09:59:45 +0100

    Filer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { MainLayout } from './Layout/MainLayout'
-import Homepage from './Layout/Homepage'
-import Pallette from './Layout/Pallette'
+import Homepage from './Layout/Homepage/Homepage'
+import Pallette from './Layout/Pallette/Pallette'
 
 function App() {
  
