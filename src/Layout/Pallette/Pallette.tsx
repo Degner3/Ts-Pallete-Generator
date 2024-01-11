c9528361c85b9e6ec64c6113a6b58006517efbe8
Degner3 <niklaschristiansen@live.dk>
2024-01-11 12:21:46 +0100

    Save corlors + Delete

--- src/Layout/Pallette/Pallette.tsx
+++ src/Layout/Pallette/Pallette.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import Button from "../../components/Button/Button"
+import { useColors } from "../../store/useColors"
+import style from "./Palette.module.scss"
 
 export default function Pallette() {
+
+  const {setColor, savedColors, deleteColor, currentColors, saveColor} = useColors()
+
+  const handleDelete = (colors) => {
+    deleteColor(colors);
+  }
+  
+
+
+  
   return (
-    <div>Pallette</div>
-  )
+    <section className={style.content}>
+      <h1
+        key={currentColors ? `colored ${currentColors[0]}` : "not colored"}
+        style={{
+          background: currentColors[0]
+            ? `-webkit-linear-gradient(0deg, ${currentColors[0]} 26.79%, ${currentColors[2]} 49.8%,${currentColors[4]} 70.09%)`
+            : "white",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+        }}
+      >
+        My Palettes
+      </h1>
+      <div className={style.container}>
+        {savedColors.map((colors, index) => (
+          <div key={index} className={style.colors}>
+            {colors.map((color, i) => (
+              <div key={i} style={{ backgroundColor: color }}></div>
+            ))}
+            <div className={style.buttonGroup}>
+              <Button>Set active</Button>
+              <Button onClick={() => handleDelete(colors)}>Delete</Button>
+            </div>
+          </div>
+        ))}
+
+        {/* <div className={style.buttonGroup}>
+          <Button>Set active</Button>
+          <Button>Delete</Button>
+        </div> */}
+      </div>
+    </section>
+  );
 }
